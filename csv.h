536e519751e9bb78e2544cd2570cd5aa87ad3f5d
devSushmita <sushmita7156@gmail.com>
2024-06-28 14:52:43 +0530

    Partially add value parsing logic

--- csv.h
+++ csv.h
@@ -2,76 +2,164 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define NEWLINE_CHARACTER 10
+#define COMMA_CHARACTER 44
+
 typedef long long int bigint;
 
-enum CsvHeaderType { 
-    TEXT,
-    INT,
-    FLOAT,
-    DATE,
-    TIME,
-    DATETIME,
-    BOOLEAN
-} typedef CsvHeaderType;
+enum CsvHeaderType {
+  TEXT,
+  INT,
+  FLOAT,
+  DATE,
+  TIME,
+  DATETIME,
+  BOOLEAN
+}
+typedef CsvHeaderType;
 
-enum Encoding {  
-    ASCII,
-    UTF8,
-    ISO8859_1,
-    WINDOWS1252
-} typedef Encoding;
+enum Encoding {
+  ASCII,
+  UTF8,
+  ISO8859_1,
+  WINDOWS1252
+}
+typedef Encoding;
 
 struct CsvHeader {
-    char *name;
-    CsvHeaderType type;
-} typedef CsvHeader;
+  char * name;
+  CsvHeaderType type;
+}
+typedef CsvHeader;
 
 struct CsvColumn {
-    CsvHeader header;
-    char **values;
-} typedef CsvColumn;
+  CsvHeader header;
+  char ** values;
+}
+typedef CsvColumn;
 
-struct Csv {    
-    CsvColumn *columns;
-    char *fileName;
-    char *filePath;
-    Encoding encoding;
-    bigint totalRows;
-    bigint totalColumns;
-} typedef Csv;
+struct Csv {
+  CsvColumn * columns;
+  char * fileName;
+  char * filePath;
+  Encoding encoding;
+  bigint totalRows;
+  bigint totalColumns;
+}
+typedef Csv;
 
-bool endsWith(char *template, char *suffix) {    
-    int templateLength = strlen(template);
-    int suffixLength = strlen(suffix);
-    return templateLength >= suffixLength && strcmp(template + templateLength - suffixLength, suffix) == 0;
+bool endsWith(char * template, char * suffix) {
+  int templateLength = strlen(template);
+  int suffixLength = strlen(suffix);
+  return templateLength >= suffixLength && strcmp(template + templateLength - suffixLength, suffix) == 0;
 }
 
-Csv *parse(const char *filePath) {
-    if(filePath) {
-        if (endsWith(filePath, ".csv")) {
-            FILE *fp = fopen(filePath, "r");
-            if (fp) {
-                Csv *csv = (Csv *) malloc(sizeof(Csv));
-                if (csv)
-                {   
-                    while(getc(fp) != ','){    
-                        
-                    }
+Csv * parse(const char * filePath) {
+  if (filePath) {
+    if (endsWith(filePath, ".csv")) {
+      FILE * fp = fopen(filePath, "r");
+      if (fp) {
+        Csv * csv = (Csv * ) malloc(sizeof(Csv));
+        if (csv) {
+          int headerLength = 0;
+          char * data = NULL;
+          csv -> totalRows = 0;
+          csv -> totalColumns = 0;
+
+          // abc,def,ghi\n
+          // header parsing logic
+          while (true) {
+            int code = fgetc(fp);
+            if (code == COMMA_CHARACTER || code == NEWLINE_CHARACTER) {
+              data = (char * ) realloc(data, (headerLength + 1) * sizeof(char));
+              data[headerLength] = '\0';
+              if (csv -> totalColumns == 0) {
+                csv -> columns = (CsvColumn * ) malloc(sizeof(CsvColumn));
+                if (!csv -> columns) {
+                  // handle error
                 }
-                else {   
-                    // error handling
+              } else {
+                csv -> columns = (CsvColumn * ) realloc(data, (csv -> totalColumns + 1) * sizeof(CsvColumn));
+                if (!csv -> columns) {
+                  // handle error
                 }
+              }
+              csv -> columns[csv -> totalColumns].header.name = data;
+              csv -> columns[csv -> totalColumns].header.type = TEXT;
+              csv -> columns[csv -> totalColumns].values = NULL;
+              csv -> totalColumns++;
+              headerLength = 0;
+              if (code == NEWLINE_CHARACTER) {
+                break;
+              }
+            } else {
+              if (headerLength == 0) {
+                data = (char * ) malloc(sizeof(char));
+                if (data) {
+                  data[headerLength] = (char) code;
+                } else {
+                  // handle error
+                }
+              } else {
+                data = (char * ) realloc(data, (headerLength + 1) * sizeof(char));
+                if (data) {
+                  data[headerLength] = (char) code;
+                } else {
+                  // handle error
+                }
+              }
+              headerLength++;
             }
-            else {  
-                // error handling
+          }
+
+          // value parsing logic
+          int code, valueCount = 0, valueLength = 0, index;
+          char * value = NULL;
+          while ((code = fgetc(fp)) != EOF) {
+            {
+              if (code == COMMA_CHARACTER || code == NEWLINE_CHARACTER) {
+                value = (char * ) realloc(value, (valueLength + 1) * sizeof(char));
+                if (value) {
+                  value[valueLength] = '\0';
+                  int colindex = valueCount % csv -> totalColumns;
+                  // store inside column's values property (csv->columns[colindex].values)
+                  if (code == NEWLINE_CHARACTER) {
+                    csv -> totalRows++;
+                  }
+                  valueLength = 0;
+                  valueCount++;
+                } else {
+                  // error handle
+                }
+              } else {
+                if (valueLength == 0) {
+                  value = (char * ) malloc(sizeof(char));
+                  if (value) {
+                    value[valueLength] = (char) code;
+                  } else {
+                    // handle error
+                  }
+                } else {
+                  value = (char * ) realloc(value, (valueLength + 1) * sizeof(char));
+                  if (value) {
+                    value[valueLength] = (char) code;
+                  } else {
+                    // handle error
+                  }
+                }
+              }
             }
+          }
+        } else {
+          // error handling
         }
-        else {  
-            // error handling
-        }
-    }
-    else {
+      } else {
         // error handling
+      }
+    } else {
+      // error handling
     }
+  } else {
+    // error handling
+  }
 }
-    
